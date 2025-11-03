bbb632bac21791339edae6c1028d28a451021895
maurya-ayush <ayushmaurya2468@gmail.com>
2025-11-03 16:03:51 +0530

    Build Try 2

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,6 +1,6 @@
 
-const baseURL = import.meta.env.PROD
-    ? 'https://api.sohocomedyhouse.com/'
+const baseURL = import.meta.env.VITE_API_BASE_URL || ''
+    ? 'https://api.sohocomedyhouse.com'
     : '/api';
 
 export default baseURL
