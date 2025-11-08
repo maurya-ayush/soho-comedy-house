90fd0ec94ea7d9a8812eb1ce427281bc4a3677d0
maurya-ayush <ayushmaurya2468@gmail.com>
2025-11-08 21:27:28 +0530

    Fixed BaseURL

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,4 +1,5 @@
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8787'
-
+const baseURL = import.meta.env.VITE_API_BASE_URL || ''
+    ? 'https://api.sohocomedyhouse.com'
+    : '/api';
 export default baseURL
