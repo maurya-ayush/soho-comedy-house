3e592df57f50676e0a6d28b01268e6ee454d3b32
maurya-ayush <ayushmaurya2468@gmail.com>
2025-11-13 21:25:05 +0530

    Fixed a Bug

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import { Instagram, Mail, MapPin } from "lucide-react"
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 
 export default function Footer() {
   return (
